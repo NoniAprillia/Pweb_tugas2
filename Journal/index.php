4da07cb3af38a546cafa22a4b52cfa44b62d5da9
NoniAprillia <165237591+NoniAprillia@users.noreply.github.com>
2024-09-14 09:39:14 +0700

    Update index.php

--- Journal/index.php
+++ Journal/index.php
@@ -34,22 +34,21 @@
     </nav>
     <div class="container text-center">
         <form method="get" action="tampil_data_journals.php">
-            <button class="btn btn-primary btn-custom" type="submit" name="action" value="tampil_data_journals">Tampilkan Data Journals</button>
+            <button class="btn btn-primary btn-custom" type="submit" name="action" value="tampil_data_journals">Tampilkan Data Jurnal </button>
         </form>
         <form method="get" action="tampil_data_journal_details.php">
-            <button class="btn btn-secondary btn-custom" type="submit" name="action" value="tampil_data_journal_details">Tampilkan Data Course Classes</button>
+            <button class="btn btn-secondary btn-custom" type="submit" name="action" value="tampil_data_journal_details">Tampilkan Data Detail Jurnal</button>
         </form>
         <form method="get" action="tampil_tanggal.php">
             <button class="btn btn-info btn-custom" type="submit" name="action" value="tampil_tanggal">Tampilkan Tanggal 13</button>
             <input type="hidden" name="tanggal" value="2024-09-13">
         </form>
         <form method="get" action="tampil_student.php">
-            <button class="btn btn-info btn-custom" type="submit" name="action" value="tampil_student">Tampilkan Student</button>
+            <button class="btn btn-info btn-custom" type="submit" name="action" value="tampil_student">Tampilkan Id Kelas Siswa</button>
             <input type="hidden" name="nama" value="44">
         </form>
     </div>
 
-    <!-- Bootstrap JS and dependencies -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-MDiK/rAhA/dAVZfzyefQZXKzRP6gXvPaZoAAnfFGRTFFNdUq5vqM+PH5mG2G9y+P" crossorigin="anonymous"></script>
 </body>
 </html>
