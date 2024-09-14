55ca8998f05ffdd4729f4023c7c7e27e5eb9dac7
NoniAprillia <165237591+NoniAprillia@users.noreply.github.com>
2024-09-14 09:39:53 +0700

    Update tampil_data_journal_details.php

--- Journal/tampil_data_journal_details.php
+++ Journal/tampil_data_journal_details.php
@@ -9,12 +9,12 @@ $data = $journalDetails->fetchData();
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Data Course Classes</title>
+    <title>Data Detail Jurnal</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet">
 </head>
 <body>
     <div class="container mt-5">
-        <h1 class="mb-4">Data Course Classes</h1>
+        <h1 class="mb-4">Data Detail Jurnal</h1>
         <table class="table table-striped table-bordered">
             <thead class="table-dark">
                 <tr>
